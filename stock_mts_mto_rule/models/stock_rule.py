ae61c1b34462ae28ebc4f072621416bc113962f8
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-10-20 12:49:58 -0500

    [FIX] stock_mts_mto_rule: consider case when group is not in values

--- stock_mts_mto_rule/models/stock_rule.py
+++ stock_mts_mto_rule/models/stock_rule.py
@@ -92,11 +92,12 @@ class StockRule(models.Model):
                 # Search all confirmed stock_moves of mts_procuremet and assign them
                 # to adjust the product's free qty
                 group_id = mts_procurement.values.get("group_id")
-                group_domain = expression.AND(
-                    [domain, [("group_id", "=", group_id.id)]]
-                )
+                if group_id:
+                    domain = expression.AND(
+                        [domain, [("group_id", "=", group_id.id)]]
+                    )
                 moves_to_assign = self.env["stock.move"].search(
-                    group_domain, order="priority desc, date asc"
+                    domain, order="priority desc, date asc"
                 )
                 moves_to_assign._action_assign()
 
